64f3e6e8d143f6db3b0917d5bbcfa56f5e3eb7f0
Scott Coursin <scott.coursin@gmail.com>
2024-02-03 11:32:10 -0600

    Integrated GoToPodium test command

---
+++ src/main/include/ISubsystemAccess.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef __SUBSYSTEMACCESS_H__
+#define __SUBSYSTEMACCESS_H__
+
+#include "subsystems/DriveSubsystem.h"
+#include "subsystems/VisionSubsystem.h"
+
+//#include <frc/DataLogManager.h>
+
+class ISubsystemAccess
+{
+public:
+    virtual DriveSubsystem&        GetDrive() = 0;
+    virtual VisionSubsystem&        GetVision() = 0;
+
+    //virtual wpi::log::DataLog&      GetLogger() = 0;
+};
+
+#endif  //ndef __SUBSYSTEMACCESS_H__
---
+++ src/main/include/subsystems/VisionSubsystem.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <vector>
+#include <wpi/DataLog.h>
+
+#include <frc2/command/SubsystemBase.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+#include <frc/smartdashboard/Field2d.h>
+#include <frc/DataLogManager.h>
+
+#include <units/angle.h>
+#include <units/length.h>
+//#include <iostream>
+
+enum BotPoseIndices
+{
+    eX
+  , eY
+  , eZ
+  , ePitch
+  , eRoll
+  , eYaw
+  , eLatency
+};
+
+class VisionSubsystem : public frc2::SubsystemBase
+{
+  public:
+    VisionSubsystem(/* args */);
+    void Periodic();
+    bool IsValid() { return m_isValid; }
+    double GetX() { return m_net_buffer[eX]; }
+    double GetY() { return m_net_buffer[eY]; }
+    double GetYaw() { return m_net_buffer[eYaw]; }
+    double GetLatency() { return m_net_buffer[eLatency]; }
+
+  private:
+    bool m_isValid = false;
+    std::vector<double> m_net_buffer{2};
+    std::vector<double> m_zero_vector = {42.0, 42.0, 42.0, 92, 10, 22};
+
+    std::shared_ptr<nt::NetworkTable> m_net_table = 
+        nt::NetworkTableInstance::GetDefault().GetTable("limelight-twoplus");
+
+  wpi::log::DoubleLogEntry m_logRobotPoseX;
+  wpi::log::DoubleLogEntry m_logRobotPoseY;
+  wpi::log::DoubleLogEntry m_logRobotPoseTheta;
+  wpi::log::DoubleLogEntry m_logLL_Latency;
+};
